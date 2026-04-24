bd883b6e79ca0a1946f2dc0146bb64490a2f7657
Mariana B. Silva <mbsil2005@gmail.com>
2026-04-24 10:32:40 -0300

    Implementação de primeiras páginas do Front-End.

---
+++ frontend/tailwind-config.js
@@ -0,0 +1,19 @@
+tailwind.config = {
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['"Playpen Sans"', 'sans-serif'],
+      },
+      colors: {
+        brand: {
+          cardBg: '#EFF3EA',
+          textDark: '#5D7D0E',
+          inputBg: '#E3F4B9',
+          inputBorder: '#8BAF35',
+          btnBg: '#8BAF35',
+          btnBorder: '#5D7D0E',
+        }
+      }
+    }
+  }
+}
